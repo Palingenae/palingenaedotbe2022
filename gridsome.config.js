ac048940a04bf77fd1ead9acc1bdf3891e13c32a
Olivia De Scheerder <olivia.descheerder@knplabs.com>
2021-04-02 18:01:32 +0200

    Installing Tailwind

--- gridsome.config.js
+++ gridsome.config.js
@@ -3,8 +3,36 @@
 
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
+const path = require ('path');
+
+function addStyleResource(rule) {
+  rule.use('style-resource')
+    .loader('style-resource-loader')
+    .options({
+      patterns: [
+        path.resolve(__dirname, './src/assets/styles/_global.scss'),
+        path.resolve(__dirname, './src/assets/styles/_typography.scss'),
+      ]
+    })
+}
+
 
 module.exports = {
   siteName: 'Palingenae',
-  plugins: []
-}
+  chainWebpack (config) {
+    const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
+
+    types.forEach(type => {
+      addStyleResource(config.module.rule('scss').oneOf(type))
+    })
+  },
+  plugins: [{
+    use: "gridsome-plugin-tailwindcss",
+    options: {
+      tailwindConfig: './tailwind.config.js',
+      presetEnvConfig: {},
+      shouldImport: true,
+      shouldTimeTravel: false
+    }
+  },
+]}
