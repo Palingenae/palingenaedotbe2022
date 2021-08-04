a35a470e62fb18477f4feb122e530a7a2f8bb46e
Olivia De Scheerder <hello@palingenae.be>
2021-08-04 22:53:21 +0200

    env: 11ty and Stylelint config

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,19 @@
+const pluginSass = require("eleventy-plugin-sass");
+
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(pluginSass, {
+    watch: "src/assets/styles/**/*.scss",
+    outputDir: "public/styles"
+  });
+
+  eleventyConfig.addPassthroughCopy("src/assets/fonts/");
+
   return {
     dir: {
-      input: "./src",
-      includes: "./src/_includes",
-      data: "./src/_data",
-      output: "./public"
+      input: "src",
+      includes: "src/_includes",
+      data: "src/_data",
+      output: "public"
     }
   };
 };
