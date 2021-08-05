2f78014d3f24e5e81bd54bf61322654aa9e8c438
Olivia De Scheerder <hello@palingenae.be>
2021-08-05 17:28:13 +0200

    fix: updating to 11ty

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,16 @@
 const pluginSass = require("eleventy-plugin-sass");
+const pluginPurgeCss = require("eleventy-plugin-purgecss");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(pluginSass, {
     watch: "src/assets/styles/**/*.scss",
-    outputDir: "public/styles"
+    outputDir: "public/assets/styles"
   });
+
+  eleventyConfig.addPlugin(pluginPurgeCss, {
+    config: "./purgecss.config.js",
+    quiet: false
+  })
 
   eleventyConfig.addPassthroughCopy("src/assets/fonts/");
 
--- src/components/SocialLinks.vue
+++ src/components/SocialLinks.vue
@@ -4,38 +4,6 @@
     </div>
 </template>
 
-<style lang="scss">
-.social-links {
-    display: flex;
-    justify-content: space-between;
-    width: 30%;
-    @include large-display {
-        width: 50%;
-    }
-    @include medium-display {
-        width: 70%;
-    }    
-    @include small-display {
-        flex-flow: column;
-        align-items: center;
-        width: 100%;
-    }
-    a {
-        font-family: $font-sans;
-        font-size: ms(1);
-        @include very-very-large-display {
-            font-size: ms(2);
-        }
-        @include very-large-display {
-            font-size: ms(1);
-        }
-        @include small-display {
-            margin-bottom: $spacing-medium
-        }
-    }
-}
-</style>
-
 <script>
 export default {
     data() {
