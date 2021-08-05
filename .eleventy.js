2e10206b6efd41047a4df75b9680293625d6282f
Olivia De Scheerder <hello@palingenae.be>
2021-08-05 23:57:01 +0200

    update: the homepage with data from JSON

--- .eleventy.js
+++ .eleventy.js
@@ -17,8 +17,8 @@ module.exports = function (eleventyConfig) {
   return {
     dir: {
       input: "src",
-      includes: "src/_includes",
-      data: "src/_data",
+      includes: "_includes",
+      data: "_data",
       output: "public"
     }
   };
